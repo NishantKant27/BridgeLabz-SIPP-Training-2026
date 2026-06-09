e733be738dfd19cd44b6e73897fc9bae95729048
NishantKant27 <nishantkant27@gmail.com>
2026-06-09 22:19:59 +0530

    [Nishant] Added: Programming Elements Level 1

---
+++ core-java-practice/gcr-codebase/programming-elements/Level1/kilometerTomiles.java
@@ -0,0 +1,7 @@
+public class kilometerTomiles {
+    public static void main(String[] args) {
+        double distance=10.8;
+        double miles_distance=distance*1.6;
+        System.out.printf("The distance 10.8 in miles is: %.2f%n", miles_distance);
+    }
+}
