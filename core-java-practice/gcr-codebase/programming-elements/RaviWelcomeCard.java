bbf043526b05c4aa67e4d23dba6f2ac0d05f7e25
NishantKant27 <nishantkant27@gmail.com>
2026-06-09 12:45:34 +0530

    [Nishant] Added: Case Study Problem of Day-1

---
+++ core-java-practice/gcr-codebase/programming-elements/RaviWelcomeCard.java
@@ -0,0 +1,19 @@
+public class RaviWelcomeCard {
+    public static void main(String[] args) {
+        String name="Ravi";
+        int age=24;
+        char rank='B';
+        double salary=20000;
+        float membership_fee=1500.0f;
+        double bonus= salary * 0.12;
+        int bonus_int=(int) salary;
+        System.out.println("****************************************");
+        System.out.println("Name           :"+name);
+        System.out.println("Age            :"+age);
+        System.out.println("Rank           :"+rank);
+        System.out.println("Salary         :"+salary);
+        System.out.println("Membership Fee :"+membership_fee);
+        System.out.println("Annual Bonus   :"+bonus_int);
+        System.out.println("****************************************");
+    }
+}
